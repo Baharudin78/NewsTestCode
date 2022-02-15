9cce1cf5d39b4d2c04af7e34e09969e5560938fe
Baharudin78 <bencoxgame78@gmail.com>
2022-02-15 14:45:32 +0700

    done

--- app/src/main/java/com/baharudin/newstestcode/ui/MainActivity.kt
+++ app/src/main/java/com/baharudin/newstestcode/ui/MainActivity.kt
@@ -1,19 +1,12 @@
-package com.baharudin.newstestcode
+package com.baharudin.newstestcode.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.baharudin.newstestcode.adapter.CategoryAdapter
+import com.baharudin.newstestcode.R
 import com.baharudin.newstestcode.databinding.ActivityMainBinding
-import com.baharudin.newstestcode.ui.NewViewModel
-import com.denzcoskun.imageslider.constants.ScaleTypes
-import com.denzcoskun.imageslider.models.SlideModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
