aa557ae697182ca5fd423818f5f499d6b528e159
Baharudin78 <bencoxgame78@gmail.com>
2022-02-15 12:04:11 +0700

    design

--- app/src/main/java/com/baharudin/newstestcode/MainActivity.kt
+++ app/src/main/java/com/baharudin/newstestcode/MainActivity.kt
@@ -2,10 +2,24 @@ package com.baharudin.newstestcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.baharudin.newstestcode.databinding.ActivityMainBinding
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageSlide = ArrayList<SlideModel>()
+        imageSlide.add(SlideModel(R.drawable.imafess))
+        imageSlide.add(SlideModel(R.drawable.imafess))
+        imageSlide.add(SlideModel(R.drawable.imafess))
+        imageSlide.add(SlideModel(R.drawable.imafess))
+        binding.ivSlider.setImageList(imageSlide,ScaleTypes.FIT)
     }
+
 }
